0ef82f806a5007ab367fbb676ed6e372fcc5d4ac
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2014-04-04 13:04:22 +0100

    NoTask - updated to use data table

--- src/main/java/skeleton/CheckOut.java
+++ src/main/java/skeleton/CheckOut.java
@@ -1,33 +1,25 @@
 package skeleton;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Created by jonathan on 04/04/2014.
  */
 public class CheckOut {
 
-    private final Map<String,Integer> productPrices = new HashMap<String,Integer>();
+    public Map<String,Integer> productPrices = new HashMap<String,Integer>();
     private int total = 0;
 
-    public void setPriceOfEspresso(int priceOfEspresso) {
-        productPrices.put("espresso", priceOfEspresso);
-    }
+   public void setProducts(Map<String,Integer> products) {
+       this.productPrices = products;
+   }
 
-    public void setPriceOfCroissant(int priceOfCroissant) {
-        productPrices.put("croissant", priceOfCroissant);
-    }
-
-    public void setQuantityOfEspresso(int quanityOfEspresso) {
-        this.total += quanityOfEspresso * productPrices.get("espresso");
-    }
-
-    public void setQuantityOfCroissant(int quanityOfCroissant) {
-        this.total += quanityOfCroissant * productPrices.get("croissant");
+    public void setQuantity(String product, int quantity) {
+        total += quantity * productPrices.get(product);
     }
 
     public int getTotal() {
-        return this.total;
+        return total;
     }
 }
--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -3,6 +3,7 @@ package skeleton;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,24 +11,14 @@ public class Stepdefs {
 
     CheckOut checkout = new CheckOut();
 
-    @Given("^an espresso costs £(\\d+)$")
-    public void an_espresso_costs_£(int price) throws Throwable {
-        checkout.setPriceOfEspresso(price);
+    @Given("^the following products:$")
+    public void the_following_products(Map<String,Integer> products) throws Throwable {
+        checkout.setProducts(products);
     }
 
-    @Given("^a croissant costs £(\\d+)$")
-    public void a_croissant_costs_£(int price) throws Throwable {
-        checkout.setPriceOfCroissant(price);
-    }
-
-    @When("^I order (\\d+) croissants$")
-    public void i_order_croissants(int quanity) throws Throwable {
-        checkout.setQuantityOfCroissant(quanity);
-    }
-
-    @When("^I order (\\d+) espresso$")
-    public void i_order_espresso(int quanity) throws Throwable {
-        checkout.setQuantityOfEspresso(quanity);
+    @When("^I order (\\d+) (.*)?")
+    public void i_order_croissants(int quantity, String product) throws Throwable {
+        checkout.setQuantity(product, quantity);
     }
 
     @Then("^the total should be £(\\d+)$")
