07a72a8905500b453725c72abedf0292eccf89c4
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2014-04-04 11:26:00 +0100

    NoTask - added first draft of new feature, step definitions and class

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -1,11 +1,27 @@
 package skeleton;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+import static org.junit.Assert.assertEquals;
 
 public class Stepdefs {
-    @Given("^I have (\\d+) cukes in my belly$")
-    public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
-        Belly belly = new Belly();
-        belly.eat(cukes);
+
+    CheckOut checkout = new CheckOut();
+
+    @Given("^an espresso costs £(\\d+)$")
+    public void an_espresso_costs_£(int price) throws Throwable {
+        checkout.setPriceOfEspresso(price);
+    }
+
+    @When("^I order (\\d+) espresso$")
+    public void i_order_espresso(int quanity) throws Throwable {
+        checkout.setQuantityOfEspresso(quanity);
+    }
+
+    @Then("^the total should be £(\\d+)$")
+    public void the_total_should_be_£(int expectedTotal) throws Throwable {
+        assertEquals("The total is incorrect", expectedTotal, checkout.getTotal());
     }
 }
